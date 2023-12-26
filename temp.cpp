c2573f950a55a8e0f39457650472d7b37125343b
sourab-jadav <sourabhjadav7@gmail.com>
2023-12-26 18:39:18 +0530

    remove files from repo cache

--- temp.cpp
+++ temp.cpp
@@ -19,33 +19,33 @@ void printBoard(int board[N][N]) {
     cout << endl;
   }
 }
-//define the issafe function which will tell if placing the queen is valid or not
+// define the issafe function which will tell if placing the queen is valid or
+// not
 
-bool issafe(int board[N][N],int x,int y){
-    //as we are placing columnwise we need to check 
-    //left side of the row
-    //left top diagonal
-    //left bottom diagonal
+bool issafe(int board[N][N], int x, int y) {
+  // as we are placing columnwise we need to check
+  // left side of the row
+  // left top diagonal
+  // left bottom diagonal
 
-    for(int i=0;i<x;i++){
-        if(board[x][i]){
-            return false;
-        }
+  for (int i = 0; i < x; i++) {
+    if (board[x][i]) {
+      return false;
     }
-    //checking the left top row
-    for(int i=x,j=y;i>=0 && j>=0;i--,j--){
-        if(board[i][j]){
-            return false;
-        }
+  }
+  // checking the left top row
+  for (int i = x, j = y; i >= 0 && j >= 0; i--, j--) {
+    if (board[i][j]) {
+      return false;
     }
-    //checking the lower diagonal if it satisfies
-    for(int i=x,j=y;i<N&& j>=0;i++,j--){
-        if(board[i][j]){
-            return false;
-        }
+  }
+  // checking the lower diagonal if it satisfies
+  for (int i = x, j = y; i < N && j >= 0; i++, j--) {
+    if (board[i][j]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
-
 
 int main() {}
