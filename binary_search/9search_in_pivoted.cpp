bacfa63d0b511bb57b65d9c6d4b4314b9a2502cb
sourabh jadav <sourabhjadav7@gmail.com>
2024-06-01 16:22:08 +0530

    understanding binary search

--- binary_search/9search_in_pivoted.cpp
+++ binary_search/9search_in_pivoted.cpp
@@ -1,8 +1,8 @@
+#include <algorithm>
 #include <iostream>
 using namespace std;
-int binarySearch(int arr[],int n,int x){  // simple and fast
-    int low=0;
-    int high=n-1;
+
+int binarySearch(int arr[],int low,int high,int x){  // simple and fast
     int mid=0;
     while (low<=high) { // <= is important here if you search for value 2 in range (1,5)
         mid=(low+high)/2; // in zero based indexing bid aligns towards left part
@@ -16,10 +16,42 @@ int binarySearch(int arr[],int n,int x){  // simple and fast
     }
     return -1;
 }
+
+
+
+int get_pivot(int arr[],int n){
+    int low=0;
+    int high=n-1;
+    int mid=0;
+    while (low<=high) {
+        mid=(low+high)/2;
+        if (arr[mid]>arr[low]) {
+            low=mid+1;
+        }else if (arr[mid]<=arr[low] && arr[mid]<arr[mid-1]) {
+            return mid;
+        }else {
+            high=mid-1;
+        }
+    }
+    return -1;
+}
+
+int pivot_binary_search(int arr[],int n,int x){
+    int p= get_pivot(arr, n);
+    if (p==-1) {
+        return binarySearch(arr,0,n-1,x);
+    }else if (arr[0]<n) {
+        return binarySearch(arr, 0,p-1,x );
+    }else {
+        return binarySearch(arr, p+1, n-1, x);
+    }
+}
+
 int main() {
-    int arr[]{1,2,3,4,5};
-    // now this array is rotated you have to find an element in logn
-    int result=binarySearch(arr, 5, 5);
+    int arr[]{5,6,7,8,9,10,1,2,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int x=7;
+    int result=pivot_binary_search(arr, n, x);
     std::cout<<result<<std::endl;
     return 0;
 }
