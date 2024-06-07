dc5cc244b1078d637f17a94114ad7551810c70b5
sourabh jadav <sourabhjadav7@gmail.com>
2024-06-07 11:12:12 +0530

    count distict elements in each window of size k [window sliding]

---
+++ 1arrays/5count_distinct_elements_in_everywindow.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
+int main() {
+    int arr[]={1,2,1,3,4,3,3};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int k=4;
+    unordered_map<int, int>mp;
+    int distinct_count=0;
+    for(int i=0;i<k;i++){
+        if (mp[arr[i]]==0) {
+            distinct_count++;
+        }
+        mp[arr[i]]++;
+    }
+    std::cout<<distinct_count<<" ";
+    for(int i=k;i<n;i++){
+        if(--mp[arr[i-k]]==0){
+            distinct_count--;
+        }
+        if (mp[arr[i]]==0) {
+            distinct_count++;
+        }
+        mp[arr[i]]++;
+        std::cout<<distinct_count<<" ";
+    }
+    
+}
+
+
