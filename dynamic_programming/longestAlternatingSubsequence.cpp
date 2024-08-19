74242542f13302e3e45655a651cb8dfbbe731a63
sourabh jadav <sourabhjadav7@gmail.com>
2024-08-19 12:02:48 +0530

    lskdfjsd

--- dynamic_programming/longestAlternatingSubsequence.cpp
+++ dynamic_programming/longestAlternatingSubsequence.cpp
@@ -24,32 +24,31 @@ using namespace std;
 //
 //
 // let's try to solve this more intuitively
-int solveRecursive(int arr[],int i,int n,bool less_than,bool greater_than){
-    if (i==n-1) {
-        return 0;
-    }
-    int result1=0;
-    int result2=0;
-    if (less_than) {
-        if (arr[i]<arr[i+1]) {
-            result1=1+solveRecursive(arr, i+1, n, false, true);
+int solveRecursive(int arr[],int i,int prev,int n,bool less_than,bool greater_than){
+
+
+    int res1=0;
+    int res2=0;
+    if (prev==-1) {
+        return max(1+solveRecursive(arr, i+1, arr[i], n, true, false),1+solveRecursive(arr,i+1,arr[i],n,false,true));
+    }else if (less_than) {
+        if (arr[i] > prev ) {
+            res1=max(1+solveRecursive(arr, i+1, arr[i], n, false,true),solveRecursive(arr, i+1, prev, n, less_than, greater_than));
         }else {
-            result2=1+solveRecursive(arr, i+1, n, true, false);
-        }
-    }else if (greater_than) {
-        if (arr[i]>arr[i+1]) {
-            result1=1+solveRecursive(arr, i+1, n, true, false);
-        }else {
-            result2=1+solveRecursive(arr, i+1, n, false, true);
+            res2=solveRecursive(arr, i+1, prev, n, less_than, greater_than);
         }
     }
-    return max(result1,result2);
+    
+
 }
+
+ 
+
 
 int main() {
     int arr[] = {10, 22, 9, 33, 49, 50, 31, 60};
     int n=sizeof(arr)/sizeof(arr[0]);
-    int result=solveRecursive(arr, 0, n, true, false);
+    int result;
     std::cout<<result<<std::endl;
     return 0;
 }
