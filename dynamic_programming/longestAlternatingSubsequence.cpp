972afb9d8b4d778c2cbe5731596b65bf33930803
sourabh jadav <sourabhjadav7@gmail.com>
2024-08-20 13:18:46 +0530

    longest alternating subseq [ dp ]

--- dynamic_programming/longestAlternatingSubsequence.cpp
+++ dynamic_programming/longestAlternatingSubsequence.cpp
@@ -1,4 +1,6 @@
 #include <algorithm>
+#include <climits>
+#include <cstring>
 #include <iostream>
 #include <vector>
 #include <string>
@@ -24,31 +26,66 @@ using namespace std;
 //
 //
 // let's try to solve this more intuitively
-int solveRecursive(int arr[],int i,int prev,int n,bool less_than,bool greater_than){
 
+// taken from internet
+// int lasEndingHere(vector<int>& arr, int i, bool isPositive) {
+//     if (i == 0) return 1;
+//     int maxLength = 1;
+//     for (int j = 0; j < i; j++) {
+//         if (isPositive && arr[j] < arr[i]) {
+//             maxLength = max(maxLength, 1 + lasEndingHere(arr, j, !isPositive));
+//         } else if (!isPositive && arr[j] > arr[i]) {
+//             maxLength = max(maxLength, 1 + lasEndingHere(arr, j, !isPositive));
+//         }
+//     }
+//
+//     return maxLength;
+// }
+//
+// int longestAlternatingSubsequence(vector<int>& arr) {
+//     int maxLength = 1;
+//     for (int i = 0; i < arr.size(); i++) {
+//         maxLength = max(maxLength, lasEndingHere(arr, i, true));
+//         maxLength = max(maxLength, lasEndingHere(arr, i, false));
+//     }
+//     return maxLength;
+// }
+//
 
-    int res1=0;
-    int res2=0;
-    if (prev==-1) {
-        return max(1+solveRecursive(arr, i+1, arr[i], n, true, false),1+solveRecursive(arr,i+1,arr[i],n,false,true));
-    }else if (less_than) {
-        if (arr[i] > prev ) {
-            res1=max(1+solveRecursive(arr, i+1, arr[i], n, false,true),solveRecursive(arr, i+1, prev, n, less_than, greater_than));
-        }else {
-            res2=solveRecursive(arr, i+1, prev, n, less_than, greater_than);
+int longestAlternatingSubsequence(vector<int>arr){
+    int n=arr.size();
+    std::cout<<"dynamic programming based solution to the problem"<<std::endl;
+    int las[n][2];
+    memset(las, 0, sizeof(las));
+    // las[i][0] this computes length of the longest alternating subsequence ending at index i 
+    // and the last element is greater than it's previous element
+
+    // las[i][1] this computes the length of the longest alternating subsequence ending at index i 
+    // and the last element is less than it's previous element
+    for(int i=0;i<n;i++){
+        las[i][0]=las[i][1]=1;
+    }
+    int res=INT_MIN;
+
+    for(int i=1;i<n;i++){
+        for(int j=0;j<i;j++){
+            if (arr[j]<arr[i] and las[i][0] < las[j][1]+1) {
+                las[i][0]=las[j][1]+1;
+            }
+            if (arr[j]>arr[i] and las[i][1] < las[j][0]+1) {
+                las[i][1]=las[j][0]+1;
+            }
+            if (res<max(las[i][0],las[i][1])) {
+                res=max(las[i][0],las[i][1]);
+            }
         }
     }
-    
-
+    return res;
 }
-
- 
 
 
 int main() {
-    int arr[] = {10, 22, 9, 33, 49, 50, 31, 60};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    int result;
-    std::cout<<result<<std::endl;
+    vector<int> arr = { 10, 22, 9, 33, 49, 50, 31, 60 };
+    cout << "Length of Longest Alternating Subsequence: " << longestAlternatingSubsequence(arr) << endl;
     return 0;
 }
