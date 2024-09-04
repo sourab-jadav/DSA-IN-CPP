7ba26f97b31654d3b0f15225fc7828c61fb81bb9
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-04 13:39:44 +0530

    initial

---
+++ dynamic_programming/weightedJobScheduling.cpp
@@ -0,0 +1,52 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+#define printxyz(arr, rows, cols)                      \
+    do {                                               \
+        std::cout<<std::endl;                          \
+        for (int i = 0; i < rows; ++i) {               \
+            for (int j = 0; j < cols; ++j) {           \
+                std::cout << arr[i][j] << " ";         \
+            }                                          \
+            std::cout << std::endl;                    \
+        }                                              \
+        std::cout<<std::endl;                          \
+    } while (0)
+#define printarray(arr, n)                             \
+        std::cout<<std::endl;                          \
+        for (int i = 0; i < n; ++i) {                  \
+            std::cout<<arr[i]<<" ";                    \
+        }                                              \
+        std::cout<<std::endl;                          \
+
+// A job has start time, finish time and profit.
+struct Job
+{
+    int start, finish, profit;
+};
+
+int solve(Job arr[],int i,int low,int high,int n){
+    if (i==n) {
+        return 0;
+    }
+
+    // the funda of including the current job and not including the current job
+    // if you include the currrent job you add it's value 
+    //  - you will include only if it does not overlap with the interval of low and high
+    // if you don't include the current job just do i+1
+
+}
+
+int findMaxProfit(Job arr[],int n){
+    return max(solve(arr, 1, arr[0].start, arr[0].finish,n)+arr[0].profit,solve(arr, 1, -1, -1,n));
+}
+ 
+int main()
+{
+    Job arr[] = {{3, 10, 20}, {1, 2, 50}, {6, 19, 100}, {2, 100, 200}};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    cout << "The optimal profit is " << findMaxProfit(arr, n);
+    return 0;
+}
