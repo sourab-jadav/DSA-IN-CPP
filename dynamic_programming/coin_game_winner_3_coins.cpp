fa1b827dd6e044492db0b2b4ceeaf0852062305b
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-10 14:08:41 +0530

    i don't see any problem but not working

--- dynamic_programming/coin_game_winner_3_coins.cpp
+++ dynamic_programming/coin_game_winner_3_coins.cpp
@@ -22,7 +22,7 @@ using namespace std;
         std::cout<<std::endl;                          \
 
 
-bool solve(int x,int y,int n,bool isA){
+int solve(int x,int y,int n,bool isA){
     if (n==1) {
         if (isA) {
             return true;
@@ -30,29 +30,36 @@ bool solve(int x,int y,int n,bool isA){
             return false;
         }
     }
-    if (n-x<0 and n-y <0) {
-        return solve(x, y, n-1, !isA);
-    }else if (n-x==0 or n-y==0) {
+    if (n==0 ) {
         if (isA) {
-            return 1;
+            return false;
         }else {
-            return 0;
+            return true;
         }
-    }else {
-        // what if n-y is greater than 0 and n-x <0
-        return (n-x>0)?solve(x, y , n-x,!isA):0 or (n-y>0)?solve(x, y, n-y,!isA):0 or solve(x, y, n-1, !isA);
     }
+    if (n-x==0 or n-y==0 ) {
+        if (isA) {
+            return true;
+        }else {
+            return false;
+        }
+    }
+    if (n-x==1 or n-y==1) {
+        if (isA) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+    return (n-1>=0)?solve(x, y, n-1, !isA):false or (n-x>=0)?solve(x, y, n-x, !isA):false or (n-y>=0)?solve(x, y, n-y, !isA):false;
+    // main is you can subtract it from 1 or from x or from y
 }
 int solveProblem(int x,int y,int n){
-    // first turn is A's 
-    // if n-x is <0 and n-y is <0 then we call for n-1 recursively
-    if (n-x<0 and n-y<0) {
-        return solve(x, y, n-1, false);
-    }else if (n-x==0 or n-y==0) {
+    bool result=solve(x, y, n, true);
+    if (result) {
         return 1;
     }else {
-        // what if n-y is greater than 0 and n-x <0
-        return (n-x>0)?solve(x, y , n-x,false):0 or (n-y>0)?solve(x, y, n-y,false):0 or solve(x, y, n-1, false);
+        return 0;
     }
 }
 
@@ -61,10 +68,16 @@ int main() {
     // int n=2;
     // int x=3;
     // int y=4 ;
-    int n=82;
-    int x=18;
-    int y=40 ;
-    // given 5 coins every player can pick 1 or 3 or 4 coins on his or her turn 
+    // int n=82;
+    // int x=18;
+    // int y=40 ;
+    int n=5;
+    int x=3;
+    int y=4;
+    // int n=9;
+    // int x=1;
+    // int y=8;
+    // given 5 coins every player can pick 1 or 3 or 4 coins on his turn 
     // the game always starts with A 
     // player who is not able to pick any coin loses the game 
     // 
