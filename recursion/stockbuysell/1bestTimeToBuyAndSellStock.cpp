0587961f9c026bdf8bd39e1a4909c48447e834f7
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-17 23:43:08 +0530

    expected is 8 but getting 23

--- recursion/stockbuysell/1bestTimeToBuyAndSellStock.cpp
+++ recursion/stockbuysell/1bestTimeToBuyAndSellStock.cpp
@@ -106,16 +106,45 @@ int k_transactions_allowed(int arr[],int start,int end,int k,int n ){
     return result;
 }
 // using the statemachine to solve the  problem
+// manageting the state of when the person bought and notbought seperately 
+// if you didn't bought the stock then 
+//      you can buy the stock or you don't buy a stock 
+//      when you buy a stock the actual value will be decreased by the price at current i and transaction fee
+// if you bought the stock then 
+//      you can sell the stock or don't sell the stock 
+//      if you sell the stock the the gain in value is price at i 
+//      you don't sell at the current i you call for i+1 value
+int statemachine_method(int arr[],int i,int n,bool bought,int fee){
+    if (i==n) { // when there is nomore stock to buy then 
+        return 0;
+    }
+    if (!bought) {
+        int opt1=(-arr[i]-fee)+statemachine_method(arr, i+1, n, true, fee);
+        int opt2=statemachine_method(arr, i+1, n, false, fee);
+        return max(opt1,opt2);
+    }else {
+        // now we had bought previously now
+        int opt1=arr[i]+statemachine_method(arr, i+1, n, true, fee);
+        int opt2=statemachine_method(arr, i+1, n, false, fee);
+        return max(opt1,opt2);
+    }
+
+
+
+}
 int main() {
     // int arr[]{2,5,13,7,15,18,12,10}; // the idea here is pick every pair i,j where j is > i 
     // int arr[]  {2, 30, 15, 10, 8, 25, 80}; 
-    int arr[]{ 10, 22, 5, 75, 65, 80}; // passed
+    // int arr[]{ 10, 22, 5, 75, 65, 80}; // passed
     // int arr[]{ 12, 14, 17, 10, 14, 13, 12, 15}; //passed
     // int arr[]{  100, 30, 15, 10, 8, 25, 80}; //passed
     // int arr[]{90,80,70,60,50};  //passed
+    int arr[]{ 1,3,2,8,4,9};
     int n=sizeof(arr)/sizeof(arr[0]);
+    int fee=2;
     // int result=findBestTimeToBuyStock(arr,0,n-1);
-    int result=k_transactions_allowed(arr,0, n-1,2 ,n);
+    // int result=k_transactions_allowed(arr,0, n-1,2 ,n);
+    int result=statemachine_method(arr, 0, n, false, fee); // expected is 8 but getting 23 for somereason
     std::cout<<result<<std::endl;
     return 0;
 }
