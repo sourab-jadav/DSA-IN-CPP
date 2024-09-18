154af6fb4f3757094db6f43fd9640e30a531ebb7
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-18 14:11:04 +0530

    passed the current test cases

--- dynamic_programming/optimal_strategy_for_game.cpp
+++ dynamic_programming/optimal_strategy_for_game.cpp
@@ -40,28 +40,59 @@ using namespace std;
 //                                                                                                // all the elements
 //                                                                                                // max(25,25);
 // }
-int max_value(int arr[],int start,int end,int n){ // not working 
-    if (start==n || end<0) {
+// int max_value(int arr[],int start,int end,int n){ // not working 
+//     if (start==n || end<0) {
+//         return 0;
+//     }
+//     if (start==end) {
+//         return arr[start];
+//     }
+//
+//     if (arr[start]+max_value(arr, start+1, end,n) > max_value(arr, start+1, end,n)) {
+//         return arr[start]+max_value(arr, start+1, end,n);
+//     }
+//
+//     if (arr[end]+max_value(arr, start, end-1,n)> max_value(arr, start, end-1,n)) {
+//         return arr[end]+max_value(arr, start, end-1,n);
+//     }
+//     return 0;
+//
+// }
+int max_value(int arr[],int start,int end,int n,bool isme){
+    if (start==n || end <0) {
         return 0;
     }
     if (start==end) {
-        return arr[start];
+        if (isme) {
+            return arr[start];
+        }else {
+            return 0;
+        }
     }
-
-    if (arr[start]+max_value(arr, start+1, end,n) > max_value(arr, start+1, end,n)) {
-        return arr[start]+max_value(arr, start+1, end,n);
+    int result1=0;
+    int result2=0;
+    int isme_result=0;
+    if (isme) {
+            result1=(arr[start]+max_value(arr, start+1, end, n,false));
+            result2=arr[end]+max_value(arr, start, end-1, n,false);
+            isme_result=max(result1,result2);
+    }else {
+        if (arr[start]>arr[end]) {
+            return max_value(arr, start+1, end, n, true);
+        }else {
+            return max_value(arr, start, end-1, n, true);
+        }
     }
-
-    if (arr[end]+max_value(arr, start, end-1,n)> max_value(arr, start, end-1,n)) {
-        return arr[end]+max_value(arr, start, end-1,n);
-    }
-    return 0;
-
+    return isme_result;
+    
 }
 int main() {
-    int arr[]{5,3,7,10};
+    // int arr[]{8,15,3,7}; // passed
+    // int arr[]{5,3,7,10};// passed
+    // int arr[]{20,30,2,2,2,10};// passed
+    int arr[]{2,2,2,2}; // passed
     int n=sizeof(arr)/sizeof(arr[0]);
-    int result=max_value(arr,0,n-1,n);
+    int result=max_value(arr,0,n-1,n,true);
     std::cout<<result<<std::endl;
 
 }
