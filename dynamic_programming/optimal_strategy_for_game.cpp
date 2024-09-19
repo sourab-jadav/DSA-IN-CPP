be857189c268dbeeaf679e5da20be3c72520ede5
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-19 12:40:50 +0530

    optimal strategy working [ dp ] [ recursion ]

--- dynamic_programming/optimal_strategy_for_game.cpp
+++ dynamic_programming/optimal_strategy_for_game.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <iostream>
+#include <map>
 #include <vector>
 #include <string>
 using namespace std;
@@ -21,85 +22,81 @@ using namespace std;
         }                                              \
         std::cout<<std::endl;                          \
 // dynamic_programming/coin_game_winner_3_coins.cpp
-// 
-// two players 
-// coins are arranged in a row 
-// a player can pick leftmost or rightmost coin and remove it from the row
-// you start the game first.
-// determine the max value you can make by selecting the leftmost or rightmost coin first and win the game
-// int max_value(int arr[],int start,int end){ // not working
-//     // what if there is only one coin
-//     if (start==end) {
-//         return arr[start];
-//     }
-//     // what if there is two or more coins
-//     // max of max value we can get by selecting the leftmost coin or 
-//     // selecting the rightmost coin
-//     return max(arr[start]+max_value(arr, start+1, end),arr[end]+max_value(arr, start, end-1)); // doing nothing 
-//                                                                                                // just finding the sum of
-//                                                                                                // all the elements
-//                                                                                                // max(25,25);
-// }
-// int max_value(int arr[],int start,int end,int n){ // not working 
-//     if (start==n || end<0) {
+
+
+//  when we think about the recursion we think about the sub problem
+//  in the given problem first turn is A's turn 
+//  when you call the subproblem the turn goes to B 
+//  since B is playing as smartly as A 
+//  it is going to select in such a way that minimum value sum comes to A
+//
+//  now say leftmost coins are vi and vi+1 
+//  and rightmost coins are vj
+//  
+//  if A pick vi then options left for B are vi+1 and vj 
+//  if B picks vi+1 then the range of options left for A will be vi+2 to vj 
+//  if B picks vj then the range of options left for A will be vi+1 to vj-1
+//  since B is playing as smart as A ,B will pick in such a way that the sum of coins A gets is minimum
+//
+//  if A picks vj then options left for B are from vi to vj-1
+//  if B picks vi then the range of options left for A will be vi+1 to vj-1
+//  if B picks vj-1 then the range of options left for A will be vi to vj-2
+//
+//
+map<vector<int>, int>memo;
+
+// int max_value(int arr[],int start,int end,int n){ // working solution
+//     // the base cases can be 
+//     // when there is only one coin
+//     if (start==n || end <0) {
 //         return 0;
 //     }
 //     if (start==end) {
 //         return arr[start];
+//     }else if (start+1 ==end) {
+//         return max(arr[start],arr[end]);
 //     }
-//
-//     if (arr[start]+max_value(arr, start+1, end,n) > max_value(arr, start+1, end,n)) {
-//         return arr[start]+max_value(arr, start+1, end,n);
+//     vector<int>k{start,end};
+//     if (memo[k]!=0) {
+//         return memo[k];
 //     }
-//
-//     if (arr[end]+max_value(arr, start, end-1,n)> max_value(arr, start, end-1,n)) {
-//         return arr[end]+max_value(arr, start, end-1,n);
-//     }
-//     return 0;
+//     // let A picks vi
+//     // and minimum of B picking vi+1 and B picking vj
+//     int pick_left=arr[start]+min(max_value(arr, start+2, end, n),max_value(arr, start+1, end-1, n));
+//     // let A picks vj 
+//     // and minimum of B picking vj-1  and B picking vi
+//    int pick_right=arr[end]+min(max_value(arr, start, end-2, n),max_value(arr, start+1, end-1, n));
+//    return memo[k]=max(pick_left,pick_right);
 // }
-long long  max_value(int arr[],int start,int end,int n,bool isA){
-    if (start==n || end <0) {
-        return 0;
-    }
-    if (start==end) {
-        if (isA) {
-            return arr[start];
-        }else {
-            return 0;
+
+
+// tabulizing the solution
+int max_value(int arr[],int start,int end,int n){ // working solution
+    int table[n][n];
+    for(int gap=0;gap<n;++gap){
+        for(int i=0,j=gap;j<n;i++,j++){
+            // here x is value of f(i+2,j)
+            // and y is value of f(i+1,j-1)
+            // and z is value of f(i,j-2)
+            //
+            int x=((i+2)<=j)?table[i+2][j]:0;
+            int y=((i+1)<=(j-1))?table[i+1][j-1]:0;
+            int z=(i<=(j-2))?table[i][j-2]:0;
+            table[i][j]=max(arr[i]+min(x,y),arr[j]+min(y,z));
         }
     }
-    long long  result1=0;
-    long long result2=0;
-    long long isA_result=0;
-    if (isA) {
-            result1=(arr[start]+max_value(arr, start+1, end, n,false));
-            result2=arr[end]+max_value(arr, start, end-1, n,false);
-            isA_result=max(result1,result2);
-    }else {
-        if (arr[start]>arr[end]) { // this if else logic is not correct and got failed for some input stl/fileInput.txt
-                                   // all though below mentioned tc's passed. here you have not handled the case of B 
-                                         // is playing as smartly as A,very well
-            return max_value(arr, start+1, end, n, true);
-        }else {
-            return max_value(arr, start, end-1, n, true);
-        }
-    }
-    return isA_result;
+    return table[0][n-1];
 }
-
-
-
-
 
 int main() {
 //     int arr[]{
 // 5182,4596,4284,3795,6756,5167,5484,634,9364,137,776,7467,9023,4808,9384,5803,2573,9776,882,3018,7742,8307,7963,4387,9402,2827,5742,5685,4144,3668,8156,4452,9808,4571,1296,2437,6246,8771,8950,8695,8126,9865,9954,5347,3211,3067,5645,7663,6679,2691,2796
-// }; // failed
-    int arr[]{5,3,7,10};// passed
-    // int arr[]{20,30,2,2,2,10};// passed
-    // int arr[]{2,2,2,2}; // passed
+// }; 
+    int arr[]{5,3,7,10};
+    // int arr[]{20,30,2,2,2,10};
+    // int arr[]{2,2,2,2}; 
     int n=sizeof(arr)/sizeof(arr[0]);
-    int result=max_value(arr,0,n-1,n,true);
+    int result=max_value(arr,0,n-1,n);
     std::cout<<result<<std::endl;
 
 }
