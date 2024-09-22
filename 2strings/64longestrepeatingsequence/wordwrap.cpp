699d18087d464a9c80b5b36c42b4f57af2d9016c
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-22 15:39:22 +0530

    clean all the tries

--- 2strings/64longestrepeatingsequence/wordwrap.cpp
+++ 2strings/64longestrepeatingsequence/wordwrap.cpp
@@ -4,34 +4,6 @@ using namespace std;
 
 int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo);
 int square(int n) { return n * n; }
-// int solveWordWrap(int words[],int n,int k,int wordIndex,int remLength){
-//     // what we can do 
-//     // we can pick a current word 
-//     // if the remLength==k we have to pick the current word
-//     int result1=0;
-//     if (wordIndex==n-1) {
-//         return 0;
-//     }
-//     if (remLength>words[wordIndex]) { // if the word at i is subtractable
-//         // why (remLength==k)? condition 
-//         // when this condition satisfies you must select the word
-//         if (remLength==k) {
-//             // minimum of make the linebrake or don't make the linebreak
-//             // when you make the linebreak the remLength resets to k value
-//             // if you don't make the linebreak 
-//             return square(k-words[wordIndex])+solveWordWrap(words, n, k, wordIndex+1, k);
-//         }else {
-//
-//         }
-//     }
-//     // if the remaining length is less than k 
-//     // then you have two options you either pick it 
-//     // or add a break to the line by resetting the remaining length
-//     // in solving these two subproblems you have to get the minimum result
-//
-//
-// }
-
 int solveWordWrapUtil(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
     // base case for last word
     if (wordIndex == n - 1) {
@@ -55,22 +27,6 @@ int solveWordWrapUtil(int words[], int n, int length, int wordIndex, int remLeng
         return square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo);
     }
 }
-//
-//
-// int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
-//     if (memo[wordIndex][remLength] != -1) { // wordIndex 
-//         return memo[wordIndex][remLength];
-//     }
-//     memo[wordIndex][remLength] = solveWordWrapUtil(words, n, length, wordIndex, remLength, memo);
-//     return memo[wordIndex][remLength];
-// }
-//
-//
-// int solveWordWrap(int words[], int n, int k) {
-//     vector<vector<int>> memo(n, vector<int>(k + 1, -1)); // creating a memo array of size K+1 
-//     return solveWordWrapUsingMemo(words, n, k, 0, k, memo); // 
-// }
-
 
 int main() {
     int words[] = {3, 2, 2, 5};
