b33423d5cecee57322e397b1f228043f5844c9ee
sourabh jadav <sourabhjadav7@gmail.com>
2024-09-22 11:28:59 +0530

    ...

--- 2strings/64longestrepeatingsequence/wordwrap.cpp
+++ 2strings/64longestrepeatingsequence/wordwrap.cpp
@@ -1,42 +1,58 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
 int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo);
 int square(int n) { return n * n; }
+int solveWordWrap(int words[],int n,int k,int wordIndex,int remLength){
+    // what we can do 
+    // we can pick a current word 
+    // if the remLength==k we have to pick the current word
 
-
-
-int solveWordWrapUtil(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
-    // base case for last word
-    if (wordIndex == n - 1) {
-        memo[wordIndex][remLength] = words[wordIndex] < remLength ? 0 : square(remLength);
-        return memo[wordIndex][remLength];
+    int pick=0;
+    if (remLength==k) {
+        pick=square(k-words[wordIndex])+solveWordWrap(words, n, k, wordIndex+1, remLength-words[wordIndex]);
     }
-    int currWord = words[wordIndex];
-    // if word can fit in the remaining line
-    if (currWord < remLength) {
-        return min(solveWordWrapUsingMemo(words, n, length, wordIndex + 1, remLength == length ? remLength - currWord : remLength - currWord - 1, memo),
-                square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo));
-    } else {
-        // if word is kept on next line
-        return square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo);
-    }
+    // if the remaining length is less than k 
+    // then you have two options you either pick it 
+    // or add a break to the line by resetting the remaining length
+    // in solving these two subproblems you have to get the minimum result
+
+
+
 }
 
-
-int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
-    if (memo[wordIndex][remLength] != -1) { // wordIndex 
-        return memo[wordIndex][remLength];
-    }
-    memo[wordIndex][remLength] = solveWordWrapUtil(words, n, length, wordIndex, remLength, memo);
-    return memo[wordIndex][remLength];
-}
-
-
-int solveWordWrap(int words[], int n, int k) {
-    vector<vector<int>> memo(n, vector<int>(k + 1, -1)); // creating a memo array of size K+1 
-    return solveWordWrapUsingMemo(words, n, k, 0, k, memo); // 
-}
+// int solveWordWrapUtil(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
+//     // base case for last word
+//     if (wordIndex == n - 1) {
+//         memo[wordIndex][remLength] = words[wordIndex] < remLength ? 0 : square(remLength);
+//         return memo[wordIndex][remLength];
+//     }
+//     int currWord = words[wordIndex];
+//     // if word can fit in the remaining line
+//     if (currWord < remLength) {
+//         return min(solveWordWrapUsingMemo(words, n, length, wordIndex + 1, remLength == length ? remLength - currWord : remLength - currWord - 1, memo),
+//                 square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo));
+//     } else {
+//         // if word is kept on next line
+//         return square(remLength) + solveWordWrapUsingMemo(words, n, length, wordIndex + 1, length - currWord, memo);
+//     }
+// }
+//
+//
+// int solveWordWrapUsingMemo(int words[], int n, int length, int wordIndex, int remLength, vector<vector<int>> memo) {
+//     if (memo[wordIndex][remLength] != -1) { // wordIndex 
+//         return memo[wordIndex][remLength];
+//     }
+//     memo[wordIndex][remLength] = solveWordWrapUtil(words, n, length, wordIndex, remLength, memo);
+//     return memo[wordIndex][remLength];
+// }
+//
+//
+// int solveWordWrap(int words[], int n, int k) {
+//     vector<vector<int>> memo(n, vector<int>(k + 1, -1)); // creating a memo array of size K+1 
+//     return solveWordWrapUsingMemo(words, n, k, 0, k, memo); // 
+// }
 
 
 int main() {
