5ff77e48e4d0852b22a7eaf3cbfe7133bc1ed33a
sourabh jadav <sourabhjadav7@gmail.com>
2024-10-17 15:48:20 +0530

    ..:gp

--- 1arrays/addingnumbers/expandingNumbers.cpp
+++ 1arrays/addingnumbers/expandingNumbers.cpp
@@ -24,16 +24,16 @@ int main() {
     int n=4685;
     std::cout<<n%10000<<std::endl;
     std::cout<<n/1000<<std::endl;
-    // the idea is take the max possible 1 followed by n zeroes that can fit as a number in a datatype
-    //
     long long x=10000000000000000; // we have taken 16 zeroes
-    std::cout<<n%x<<std::endl;
-    std::cout<<n/x<<std::endl;
+    // std::cout<<n%x<<std::endl;
+    // std::cout<<n/x<<std::endl;
     // for(int i=1;i<18;i++){
     //     std::cout<<n%x<<std::endl;
     //     x/=10;
     // }
     string output="";
+    // the idea is take the max possible 1 followed by n zeroes that can fit as a number in a datatype
+    // 
     while (n!=0) {
         if (n/x!=0) {
             std::cout<<n%x<<std::endl;
