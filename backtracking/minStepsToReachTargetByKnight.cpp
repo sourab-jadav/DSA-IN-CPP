1f0f45370e49ee457f90cca1926eb7f0897e0900
sourabh jadav <sourabhjadav7@gmail.com>
2024-11-20 18:03:14 +0530

    problemm 2

--- backtracking/minStepsToReachTargetByKnight.cpp
+++ backtracking/minStepsToReachTargetByKnight.cpp
@@ -38,7 +38,7 @@ int recursive_sol(int kX,int kY,int tX,int tY,int dx[],int dy[],int N){
     int next_x;
     int next_y;
     int result=INT_MAX;
-    for(int i=0;i<N;i++){
+    for(int i=0;i<N;i++){ // the recursive call inside the this loop does not correctly  handle the visited states
         next_x=kX+dx[i];
         next_y=kY+dy[i];
         if (isFeasable(next_x, next_y,  N) && recursive_sol(next_x, next_y, tX,tY,dx, dy, N)) {
@@ -56,15 +56,16 @@ int recursive_sol(int kX,int kY,int tX,int tY,int dx[],int dy[],int N){
 int calculateSteps(int knightPos[],int targetPos[],int N){
     int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
     int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+    return recursive_sol(knightPos[0], knightPos[1], targetPos[0], targetPos[1], dx, dy, N);
 }
 
 
 int main() {
     // we have knight and we have the target
-   int n=30;
+   int N=30;
    int knightPos[]{0,0};
    int targetPos[]{29,29};
-   int matrix[30][30];
-   memset(matrix, 0, sizeof(matrix));
+   int result=calculateSteps(knightPos, targetPos, N);
+   std::cout<<result<<std::endl;
     return 0;
 }
