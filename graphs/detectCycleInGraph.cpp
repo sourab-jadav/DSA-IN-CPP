f63f8a352d52aae9cb5eecaeb911a0e59bdb1634
sourabh jadav <sourabhjadav7@gmail.com>
2024-11-24 00:05:41 +0530

    detect cycle in directed graph using bfs

--- graphs/detectCycleInGraph.cpp
+++ graphs/detectCycleInGraph.cpp
@@ -1,9 +1,12 @@
 #include <climits>
+#include <bits/stdc++.h>
 #include <cstdint>
 #include <iostream>
 #include <queue>
 #include <vector>
-// detecting cycle in undirected graph
+using namespace std;
+// detecting cycle in undirected graph using dfs
+//
 // bool dfs(std::vector<int> vec[], std::vector<bool> visited, int n, int s) {
 //     visited[s] = true;
 //     std::cout << s << " ";
@@ -31,7 +34,7 @@
 //
 
 
-//detecting cycle in directed graph
+//detecting cycle in directed graph using dfs
 // you can only solve dfs related questions only on how well you understood the technique
 bool dfs(std::vector<int> vec[], std::vector<bool> visited ,std::vector<bool>recstk, int s) {
     visited[s]=true;
@@ -46,7 +49,6 @@ bool dfs(std::vector<int> vec[], std::vector<bool> visited ,std::vector<bool>rec
     }
     recstk[s]=false; // this means the cycle is not formed and we are closing the recursion stack
     return false;
-
 }
 void addEdge(std::vector<int> vec[], int u, int v) {
     vec[u].push_back(v);
@@ -57,7 +59,63 @@ void createGraph(std::vector<std::vector<int>> graph, std::vector<int> vec[]) {
         addEdge(vec, graph[i][0], graph[i][1]);
     }
 }
+// 
+// detecting cycle in indirected graph using bfs 
+// the logic is similar use the bfs traversal and one visited array 
+// if you visit a vertex which is already visited and which is not u then the cycle is detected
 //
+//
+// detecting cycle in directed graph using bfs 
+// this is pretty challenging 
+// here the concept of indegree should be considered
+
+void create_indegree(vector<int>vec[],int n,vector<int>&indegree){
+   for(int i=0;i<n;i++){
+       for(auto it:vec[i]){
+           indegree[it]++;
+       }
+   }
+   // printarray(indegree, n);
+}
+bool checkCycleInDirected(vector<int>vec[],int n){
+    // the approach is to maintain a count 
+    // if V != cnt then there is cycle in the graph 
+    // if v== cnt then there is no cycle in the graph
+    // as each vertex in the cycle as atleast one degree and there is no 
+    // other way to enter that cycle 
+    //
+    // so if there is a cycle then cnt will always be less than V
+
+    vector<int>indegree(6,0);
+    create_indegree(vec, n, indegree);
+    // no that indegree of each vertex is created we create a queue and push all the vertices 
+    // who's indegree is 0
+    queue<int>q;
+    for(int i=0;i<n;i++){
+        if (indegree.at(i)==0) {
+            q.push(i);
+        }
+    }
+    int cnt=1;
+    while (!q.empty()) {
+        int el=q.front();
+        q.pop();
+        for (auto x : vec[el]) {
+            if (x==0) {
+                q.push(x);
+                cnt++;
+            }
+        }
+    }
+    if (cnt!=n) {
+        return true;
+    }else {
+        return false;
+    }
+}
+
+
+// Driver program to test above functions
 int main() {
     std::vector<std::vector<int>> graph{
         {0,1},{2,1},{2,3},{3,4},{4,5},{5,4}
@@ -68,7 +126,8 @@ int main() {
     createGraph(graph, vec);
     std::vector<bool> visited(n, false);
     std::vector<bool> recstk(n, false);
-    bool result = dfs(vec, visited,recstk, 0);
+    // bool result = dfs(vec, visited,recstk, 0);
+    bool result= checkCycleInDirected(vec, n);
     if (result) {
         std::cout << "cycle is detected" << std::endl;
     }else {
