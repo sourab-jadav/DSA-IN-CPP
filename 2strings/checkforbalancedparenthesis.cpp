e84d28c7a490eafa0bdbaf104508cdb0882cc8c6
sourabh jadav <sourabhjadav7@gmail.com>
2025-04-19 15:05:15 +0530

    balanced paranthesis  [ string ] [ stack ]

--- 2strings/checkforbalancedparenthesis.cpp
+++ 2strings/checkforbalancedparenthesis.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
+#include <stack>
 #include <string>
 
+// obeservations:
+// closing brace will only appear after opening brace
+// open brackets must  be closed in the correct order 
+//
+// ()[({)}]   -- this is not valid
+bool check_para(std::string &str){
+    // checking if the parenthesis is valid or not
+    std::stack<char>stk;
+    for(auto x :str){
+        if (x=='(' || x=='[' || x=='{') {
+            stk.push(x);
+        }else {
+            if (stk.empty() ||
+                stk.top()=='[' && x != ']' ||
+                stk.top()=='{' && x != '}' ||
+                stk.top()=='(' && x != ')' 
+                    ) return false;
+            stk.pop();
+        }
+    }
+  return true;
+}
 int main() {
     std::string str="()[{}()]";
-    // checking if the parenthesis is valid or not
-    
-    std::cout<<"what is that you want and what you care about so much"<<std::endl;
-  return 0;
+    std::cout<<check_para(str)<<std::endl;
 }
