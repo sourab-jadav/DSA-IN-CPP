ccc5296d203d7f0279536cfc5228cf7854a1dc1e
sourabh jadav <sourabhjadav7@gmail.com>
2025-04-19 15:22:11 +0530

    balanced paranthesis  [ string ] [ stack ]

--- 2strings/checkforbalancedparenthesis.cpp
+++ 2strings/checkforbalancedparenthesis.cpp
@@ -7,6 +7,17 @@
 // open brackets must  be closed in the correct order 
 //
 // ()[({)}]   -- this is not valid
+//
+// explaination:
+// since it is given that open brackets must be closed in the order 
+// meaning the recently opened bracket must be closed first
+// 
+//      to solve this we can make use of stack container and compare each closing bracket with recently opened bracket
+//      in each comparision with the closing bracket if they found to be different bracket type then the order is incorrect and you return false
+//
+//      else they must be same type and you pop the opening bracket from the stack 
+//
+//      
 bool check_para(std::string &str){
     // checking if the parenthesis is valid or not
     std::stack<char>stk;
